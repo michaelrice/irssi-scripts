57790bc175cdb3d4f5d723340ab5970b2c3b430e
Michael Rice <michael.rice@rackspace.com>
2014-01-28 03:06:04 -0600

    changes to rjbot to get it working

--- rjbot.pl
+++ rjbot.pl
@@ -1,8 +1,14 @@
+# Yo dawg. This here is a little bot like script to pull facts
+# about R to the J for irssi
+#
+# enojy
+
 use strict;
 use Irssi;
 use locale;
 use LWP;
 use JSON;
+use Data::Dumper;
 
 
 use vars qw($VERSION %IRSSI);
@@ -12,37 +18,52 @@ $VERSION = '0.9';
     author        => 'errr',
     contact       => 'code@michaelrice.org',
     name          => 'Michael Rice',
-    description   => 'client for rjfacts.pw'
+    description   => 'client for rjfacts.pw',
     license       => 'MIT',
-    url           => 'https://github.com/michaelrice/irssi-scripts'
+    url           => 'https://github.com/michaelrice/irssi-scripts',
     orignal_date  => 'Jan. 28th 2014'
 );
 
 sub sig_public {
     my ($server, $msg, $nick, $address, $target) = @_;
     return if $nick eq $server->{nick};
-    
+
     $msg =~ s/[\000-\037]//g;
     $msg = lc($msg);
     chomp($msg);
     my $win = $server->channel_find($target);
     my $fact;
 
-    if($msg =~ /\b!rjfact$/) {
+    if($msg =~ m/!rjfact$/) {
         #RUN a sub to get random fact
         $fact = get_random();
+        if($fact->code != 200) {
+            return;
+        }
+
         my $json = decode_json($fact->content);
-        $fact = $json->{'factoid'};
+        $fact = $json->{'factoid'} . " -- Fact: #" . $json->{'id'};
     }
-    elsif($msg =~ /\b!rjfact \d+/) {
+
+    elsif($msg =~ /!rjfact (\d+)$/) {
         #RUN sub to get specific rjfact 
+        $fact = "I would get info about $1, pull requests welcome!";
     }
-    elsif($msg =~ /\b!rjfact add (.+)/) {
+
+    elsif($msg =~ /!rjfact add (.+)$/) {
         # RUN sub to add new fact
+        $fact = "I would be adding the following fact if this method were implemented: $1";
     }
-    elsif($msg =~ /\b!rjfact (remove|del) \d+/) {
+
+    elsif($msg =~ /!rjfact remove (\d+)$/) {
         #RUN sub to delete a rjfact
+        $fact = "I would remove $1 from the db if I had code to do so..";
     }
+
+    elsif($msg =~ /!rjfact help$/) {
+        $fact = "!rjfact <command> <params> c[add|remove] p[factoid|factId]";
+    }
+
     return unless $fact;
     $win->command("/ ". $fact);
 }
